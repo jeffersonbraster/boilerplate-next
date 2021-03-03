c64821d4847f5be18de60001ef7c5e78cf8b421f
Jefferson <jeffersonbraster@gmail.com>
2021-03-03 19:48:49 -0300

    criando componente e config pwa

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,17 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>react avancado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="logo" />
+    <S.Title>Next Boilerplate</S.Title>
+    <S.Description>
+      Typescript, ReactJs, NextJs e Styled-components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="um desenvolvedor de frente a tela com codigo"
+    />
+  </S.Wrapper>
 )
 
 export default Main
